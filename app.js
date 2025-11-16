4a11483cf1ac09dab5146f5fc21b5c2569f21aee
Vanina Galeano <vaninagaleano21@gmail.com>
2025-11-16 12:02:45 -0300

    V0.1.20

---
+++ app.js
@@ -0,0 +1,38 @@
+const modal = document.getElementById("modal");
+const modalClose = document.getElementById("modalClose");
+const modalImg = document.getElementById("modalImg");
+const modalName = document.getElementById("modalName");
+const modalDesc = document.getElementById("modalDesc");
+const contactBtn = document.getElementById("contactBtn");
+
+// Abrir modal
+document.querySelectorAll(".ver-btn").forEach(btn => {
+  btn.addEventListener("click", e => {
+    const card = e.target.closest(".card");
+
+    const name = card.dataset.name;
+    const desc = card.dataset.desc;
+    const img = card.dataset.img;
+
+    modalImg.src = img;
+    modalName.textContent = name;
+    modalDesc.textContent = desc;
+
+    // Mensaje para WhatsApp
+    //const mensaje = encodeURIComponent(Hola, estoy interesado/a en el producto: ${name}. ¿Está disponible?);
+
+    //contactBtn.href = https://wa.me/5490000000000?text=${mensaje};
+
+    modal.classList.remove("hidden");
+  });
+});
+
+// Cerrar modal
+modalClose.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
+
+// Cerrar clickeando fuera
+modal.addEventListener("click", e => {
+  if (e.target === modal) modal.classList.add("hidden");
+});
